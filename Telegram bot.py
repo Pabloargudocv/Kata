ace04f49a6d9271f6384324160fb03b55763edbe
Pabloargudocv <65420736+Pabloargudocv@users.noreply.github.com>
2020-05-16 13:18:30 +0200

    Add files via upload

---
+++ Telegram bot.py
@@ -0,0 +1,22 @@
+from telegram.ext import Updater, CommandHandler
+
+
+def main():
+    # Instanciamos el updater
+    updater = Updater(token=open("./bot_token").read(), use_context=True)
+
+    # Añadiendo un manejador al comando /start
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+
+    # Empezamos a pedir notificaciones a Telegram
+    updater.start_polling()
+
+    # Capturamos señales de parada
+    updater.idle()
+    
+def start(update, context):
+    update.message.reply_text("Hola, soy un bot")
+    pass
+
+main()
+
